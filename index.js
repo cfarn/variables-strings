01d27d4e19ecf6b1f39e185d1496195d8ac04b04
cfarn <58476975+cfarn@users.noreply.github.com>
2022-06-20 15:15:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// 01 - Hello World
+// console.log("Hello World !")
+// 02 - String
+// const sentence = "My name is Cin"
+// console.log(sentence)
+// 03 - Concaténation
+// const name = 'Cin'
+// console.log(`Nice to meet you ${name}`)
+// 04 - String Length
+// const sentence = "I'm a new text !"
+// console.log(sentence.length)
+// 05 - Replace
+// const food = 'croissant is meh'
+// console.log(food.replace("meh", "so good"))
+// 06 - Up and Down
+// const basic = "This is cool"
+// const basicUp = basic.toUpperCase()
+// const basicDown = basic.toLowerCase()
+// console.log(`${basic} ${basicUp} ${basicDown}`)
+// 07 - Split
+// const word = "banana"
+// const letters = word.split("")
+// console.log(letters)
+// 08 - Template
+const age = 38
+const template = `I'm ${38} years old`
+console.log(template)
